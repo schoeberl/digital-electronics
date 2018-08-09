0ea5c43d8e03b0a24fe5eb3500b035ef45a4b7a7
Martin Schoeberl <martin@jopdesign.com>
2018-08-09 22:30:31 +0200

    Use std_logic_vector for the driver

--- book/section3.6/testbench.vhd
+++ book/section3.6/testbench.vhd
@@ -13,7 +13,7 @@ architecture test of testbench is
          output : out std_logic );
   end component;
   
-  signal counter: unsigned(2 downto 0);
+  signal input: std_logic_vector(2 downto 0);
   signal output: std_logic;
   
 begin
@@ -21,16 +21,16 @@ begin
   -- generate inputs and print the logic table
   process begin
     for i in 0 to 7 loop
-      counter <= to_unsigned(i, 3);
+      input <= std_logic_vector(to_unsigned(i, 3));
       wait for 10 ns;
-      report to_string(counter) & " "
+      report to_string(input) & " "
          & to_string(output);
     end loop;
     std.env.stop(0);
   end process;
   
   -- the device under test
-  dut: majority port map(counter(2), 
-            counter(1), counter(0), output);
+  dut: majority port map(input(2), 
+            input(1), input(0), output);
 
 end test;
